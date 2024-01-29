3955f9d352dac5d2aef247359f7430a8a45b4f15
ibarbylev <ibarbylev@gmail.com>
2024-01-29 22:50:17 +0200

    add bootstrap and fix index page

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,4 +4,6 @@ from recipe import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
+    path('add_product_to_recipe/<int:pk>/', views.AddIngredientToRecipeView.as_view(),
+         name='add_product_to_recipe'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .models import RecipeComposition, Recipe, Ingredient
+
 
 class HomeView(generic.TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipes'] = Recipe.objects.all()
+        context['ingredients'] = Ingredient.objects.all()
+        return context
+
+
+class AddIngredientToRecipeView(generic.FormView):
+    template_name = 'recipe/change-recipe.html'
+
+    def get(self, recipe_pk, **kwargs):
+        return render(self.request, self.template_name)
+
+    def form_valid(self, form):
+        form.instance.recipe_id = self.kwargs['recipe_id']
+        form.instance.product_id = self.kwargs['product_id']
+        form.instance.weight = self.kwargs['weight']
+        return super().form_valid(form)
